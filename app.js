366fe6758227c9f2a5f23318b0ba5543add22124
hynkififor <150060347+hynkififor@users.noreply.github.com>
2024-04-28 12:32:16 +0300

    Добавлен GET запрос по адресу "/"

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const PORT = 3000;
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.status(404).send('<h1>Страница не найдена</h1>');
+  });
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
 })
