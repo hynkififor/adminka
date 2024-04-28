428eb964a48c4243f90e228f673b96e384ce440f
hynkififor <150060347+hynkififor@users.noreply.github.com>
2024-04-28 16:30:14 +0300

    Добавлен cors

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const app = express();
 const fs = require("fs").promises;
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('./middlewares/cors');
 
 const mainRouter = require("./routes/main");
-const gamesRouter = require('./routes/games');;
+const gamesRouter = require('./routes/games');
 
 const PORT = 3000;
 
 app.use(
+  cors,
   bodyParser.json(),
   express.static(path.join(__dirname, 'public')),
   mainRouter,
