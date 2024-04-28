c6012c9ed8ccc281e4a689ceaa231d2d4ca72fcc
hynkififor <150060347+hynkififor@users.noreply.github.com>
2024-04-28 13:40:52 +0300

    Добавлены routes

--- app.js
+++ app.js
@@ -3,27 +3,15 @@ const app = express();
 const fs = require("fs").promises;
 const path = require('path');
 
+const mainRouter = require("./routes/main");
+const gamesRouter = require('./routes/games');;
+
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', async (req, res) => {
-  await fs.readFile("./public/index.html").then((data) => {
-    res.header("Content-Type", "text/html");
-    res.send(data);
-  })
-});
+app.use(mainRoute, gamesRouter);
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
+  console.log(`Server is running at PORT http://localhost:${PORT}`);
 });
-
-
-// const mainRoute = require('./routes/main');
-// const gamesRouter = require('./routes/games');
-
-// app.use(mainRoute, gamesRouter); 
-
-// app.listen(PORT, () => {
-  // console.log(`Server is running at PORT http://localhost:${PORT}`);
-// });
---
+++ routes/games.js
@@ -0,0 +1,8 @@
+const gamesRouter = require('express').Router();
+
+const getAllGames = async (req, res) => {}
+
+gamesRouter.get("/games", getAllGames)
+
+
+module.exports = gamesRouter;
