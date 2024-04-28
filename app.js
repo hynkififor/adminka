5ed307a9e49718bfd7b017ef567632b027b58160
hynkififor <150060347+hynkififor@users.noreply.github.com>
2024-04-28 13:15:39 +0300

    Добавлен фронтенд в папку public

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
 })
+
+// const mainRoute = require('./routes/main');
+// const gamesRouter = require('./routes/games');
+
+// app.use(mainRoute, gamesRouter); 
+
+// app.listen(PORT, () => {
+  // console.log(`Server is running at PORT http://localhost:${PORT}`);
+// });
